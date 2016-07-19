d7895bd0a8e940c85a3463fbaf0d3622d72d45cf
javabuk <joprieto@gmail.com>
2016-07-19 21:07:25 +0200

    Subida inicial proyecto prueba Spring JPA

--- PruebasSpringBoot/src/main/java/org/springboot/general/AplicacionController.java
+++ PruebasSpringBoot/src/main/java/org/springboot/general/AplicacionController.java
@@ -2,7 +2,11 @@ package org.springboot.general;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springboot.entidad.Persona;
 import org.springboot.entidad.Respuesta;
+import org.springboot.repositorios.RepositorioPersonas;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.support.Repositories;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +16,26 @@ public class AplicacionController {
 	private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
     
+    private RepositorioPersonas personaDao;
+    
+    @Autowired
+    public void setRepositorioPersonas(RepositorioPersonas personaRepositorio) {
+        this.personaDao = personaRepositorio;
+    }
+
+    
     @RequestMapping("/greeting")
     public Respuesta greeting(@RequestParam(value="name", defaultValue="World") String name) {
         
+    	Persona persona = new Persona();
+    	persona.setAddress("Address");
+    	persona.setCity("City");
+    	persona.setFirstName("PrimerApellido");
+    	persona.setLastName("Segundo apellido");
+    	
+    	personaDao.save(persona);
+    	
+    	
     	Respuesta resp = new Respuesta();
     	resp.setCodigo(100);
     	resp.setDescripcion("Prueba");
---
+++ PruebasSpringJPA/src/main/java/org/model/ConfigMensaje.java
@@ -0,0 +1,98 @@
+package org.model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the CONFIG_MENSAJES database table.
+ * 
+ */
+@Entity
+@Table(name="CONFIG_MENSAJES")
+@NamedQuery(name="ConfigMensaje.findAll", query="SELECT c FROM ConfigMensaje c")
+public class ConfigMensaje implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+
+	@Column(name="CORRELACION_CODIGOS")
+	private int correlacionCodigos;
+
+	@Column(name="MENSAJE_ORIGEN")
+	private String mensajeOrigen;
+
+	@Column(name="SISTEMA_DESTINO")
+	private String sistemaDestino;
+
+	@Column(name="SISTEMA_ORIGEN")
+	private String sistemaOrigen;
+
+	@Column(name="TIPO_TRAZABILIDAD")
+	private int tipoTrazabilidad;
+
+	@Column(name="TRADUCCION_MENSAJE")
+	private String traduccionMensaje;
+
+	public ConfigMensaje() {
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getCorrelacionCodigos() {
+		return this.correlacionCodigos;
+	}
+
+	public void setCorrelacionCodigos(int correlacionCodigos) {
+		this.correlacionCodigos = correlacionCodigos;
+	}
+
+	public String getMensajeOrigen() {
+		return this.mensajeOrigen;
+	}
+
+	public void setMensajeOrigen(String mensajeOrigen) {
+		this.mensajeOrigen = mensajeOrigen;
+	}
+
+	public String getSistemaDestino() {
+		return this.sistemaDestino;
+	}
+
+	public void setSistemaDestino(String sistemaDestino) {
+		this.sistemaDestino = sistemaDestino;
+	}
+
+	public String getSistemaOrigen() {
+		return this.sistemaOrigen;
+	}
+
+	public void setSistemaOrigen(String sistemaOrigen) {
+		this.sistemaOrigen = sistemaOrigen;
+	}
+
+	public int getTipoTrazabilidad() {
+		return this.tipoTrazabilidad;
+	}
+
+	public void setTipoTrazabilidad(int tipoTrazabilidad) {
+		this.tipoTrazabilidad = tipoTrazabilidad;
+	}
+
+	public String getTraduccionMensaje() {
+		return this.traduccionMensaje;
+	}
+
+	public void setTraduccionMensaje(String traduccionMensaje) {
+		this.traduccionMensaje = traduccionMensaje;
+	}
+
+}
