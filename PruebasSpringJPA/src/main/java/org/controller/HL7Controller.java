44e0d281344598ff243f277cb506239880c0e758
javabuk <joprieto@gmail.com>
2016-10-10 00:04:35 +0200

    Implementar la funcionaliad de pasar un valor de un formulario a una página de resultados.

--- PruebasSpringJPA/src/main/java/org/controller/HL7Controller.java
+++ PruebasSpringJPA/src/main/java/org/controller/HL7Controller.java
@@ -1,13 +1,27 @@
 package org.controller;
 
+import org.model.MensajeHL7;
+import org.servicios.ConfiguracionService;
+import org.servicios.GeneracionHL7Service;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HL7Controller {
 
+	
+	private GeneracionHL7Service hl7Service;
+
+    @Autowired
+    public void setCService(GeneracionHL7Service hl7Service) {
+        this.hl7Service = hl7Service;
+    }
+	
 	
 	@RequestMapping(value = "/FormularioHL7", method = RequestMethod.GET)
 	public String list(Model model) {
@@ -17,6 +31,14 @@ public class HL7Controller {
 	
 	@RequestMapping(value = "/MensajesOML", method = RequestMethod.GET)
 	public String mensajesOML(Model model) {
+		model.addAttribute("mensajeHL7", new MensajeHL7());
 		return "MensajesOML";
+	}
+	
+	@RequestMapping(value = "/ResultadoMensajesHL7", method = RequestMethod.POST)
+	public String ResultadoMensajesHL7(Model model, MensajeHL7 mensaje) {
+		mensaje.setMensaje(hl7Service.convertirMensajeOML(mensaje.getMensaje()));
+		model.addAttribute("mensaje", mensaje);
+		return "ResultadoMensajesHL7";
 	}
 }
---
+++ PruebasSpringJPA/src/main/java/org/model/MensajeHL7.java
@@ -0,0 +1,15 @@
+package org.model;
+
+public class MensajeHL7 {
+	
+	private String mensaje;
+
+	public String getMensaje() {
+		return mensaje;
+	}
+
+	public void setMensaje(String mensaje) {
+		this.mensaje = mensaje;
+	}
+
+}
