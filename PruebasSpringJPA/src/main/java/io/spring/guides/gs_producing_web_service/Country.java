2962cd9b802870c346c5be30a15d3f4a4bdf5a4f
javabuk <joprieto@gmail.com>
2016-10-12 12:58:46 +0200

    Subida después de la migración a github

--- PruebasSpringJPA/src/main/java/io/spring/guides/gs_producing_web_service/Country.java
+++ PruebasSpringJPA/src/main/java/io/spring/guides/gs_producing_web_service/Country.java
@@ -2,7 +2,7 @@
 // Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.7 
 // Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2016.10.10 a las 11:02:53 PM CEST 
+// Generado el: 2016.10.12 a las 12:49:02 PM CEST 
 //
 
 
