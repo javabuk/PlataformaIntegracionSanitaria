f4b9635f662f9f6eb9e7ecff28ea72c977b23b14
javabuk <joprieto@gmail.com>
2017-04-10 19:52:41 +0200

    Utilización de la BBDD hsqldb Pruebas de pantallas

--- backoffice-integracion-medios-diagnosticos/src/main/java/org/backoffice/controller/LoginController.java
+++ backoffice-integracion-medios-diagnosticos/src/main/java/org/backoffice/controller/LoginController.java
@@ -1,5 +1,8 @@
 package org.backoffice.controller;
 
+import org.backoffice.dao.ConfiguracionRepository;
+import org.backoffice.model.Configuracion;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +11,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginController {
 
+	@Autowired
+    ConfiguracionRepository configuracionRepository;
+	
 	@RequestMapping("/login")
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
+        
+        Configuracion nuevaConfiguracion = new Configuracion(new Long(500), "LOGIN", name);
+        configuracionRepository.save(nuevaConfiguracion);
+        
+        for (Configuracion configuracion : configuracionRepository.findAll()) {
+            System.out.println(configuracion);
+        }
+        
         return "login";
     }
 	
