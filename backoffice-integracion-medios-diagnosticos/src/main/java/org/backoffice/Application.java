b6432d333987780740fb387c2af61abe0c584f3f
javabuk <joprieto@gmail.com>
2017-07-08 15:00:33 +0200

    Incorporar opcion para crear mensajes HL7 de prueba.

--- backoffice-integracion-medios-diagnosticos/src/main/java/org/backoffice/Application.java
+++ backoffice-integracion-medios-diagnosticos/src/main/java/org/backoffice/Application.java
@@ -1,58 +1,52 @@
 package org.backoffice;
 
+import java.text.SimpleDateFormat;
 
 import org.backoffice.dao.ConfiguracionRepository;
-import org.backoffice.model.Configuracion;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
-
-import javax.sql.DataSource;
-import java.text.SimpleDateFormat;
-import java.util.stream.Stream;
-
-import static java.lang.System.exit;
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 //for jsr310 java 8 java.time.*
 //@EntityScan(
 //        basePackageClasses = { SpringBootConsoleApplication.class, Jsr310JpaConverters.class }
 //)
 @SpringBootApplication
-public class Application{
-//public class Application implements CommandLineRunner {
+@EnableWebMvc
+public class Application extends WebMvcAutoConfiguration {
+	// public class Application implements CommandLineRunner {
 
-    private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+	private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 
-    @Autowired
-    ConfiguracionRepository configuracionRepository;
+	@Autowired
+	ConfiguracionRepository configuracionRepository;
 
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(Application.class, args);
-    }
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Application.class, args);
+	}
 
-    /*
-    //@Transactional(readOnly = true)
-    @Transactional (propagation = Propagation.NEVER)
-    @Override
-    public void run(String... args) throws Exception {
-
-        //System.out.println("DATASOURCE = " + dataSource);
-
-        for (Configuracion configuracion : configuracionRepository.findAll()) {
-            System.out.println(configuracion);
-        }
-        
-        Configuracion nuevaConfiguracion = new Configuracion(new Long(400), "JORGE2", "VALOR2");
-        configuracionRepository.save(nuevaConfiguracion);
-        
-        
-
-        System.out.println("Done!");
-
-        exit(0);
-    }*/
+	/*
+	 * //@Transactional(readOnly = true)
+	 * 
+	 * @Transactional (propagation = Propagation.NEVER)
+	 * 
+	 * @Override public void run(String... args) throws Exception {
+	 * 
+	 * //System.out.println("DATASOURCE = " + dataSource);
+	 * 
+	 * for (Configuracion configuracion : configuracionRepository.findAll()) {
+	 * System.out.println(configuracion); }
+	 * 
+	 * Configuracion nuevaConfiguracion = new Configuracion(new Long(400),
+	 * "JORGE2", "VALOR2"); configuracionRepository.save(nuevaConfiguracion);
+	 * 
+	 * 
+	 * 
+	 * System.out.println("Done!");
+	 * 
+	 * exit(0); }
+	 */
 
 }
--- backoffice-integracion-medios-diagnosticos/src/main/java/org/backoffice/controller/LoginController.java
+++ backoffice-integracion-medios-diagnosticos/src/main/java/org/backoffice/controller/LoginController.java
@@ -1,17 +1,41 @@
 package org.backoffice.controller;
 
 import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.math.BigInteger;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.poi.xwpf.usermodel.ParagraphAlignment;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
+import org.apache.poi.xwpf.usermodel.XWPFTable;
+import org.apache.poi.xwpf.usermodel.XWPFTableCell;
+import org.apache.poi.xwpf.usermodel.XWPFTableRow;
 import org.backoffice.dao.CodigoRepository;
 import org.backoffice.dao.ConfiguracionRepository;
 import org.backoffice.model.Codigo;
 import org.backoffice.model.Configuracion;
 import org.backoffice.model.DatosSituacionActual;
 import org.backoffice.model.MensajeConfirmacion;
+import org.backoffice.model.MensajeHL7;
+import org.backoffice.servicios.GeneracionHL7Service;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTHeight;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTShd;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTString;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTTblPr;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTTblWidth;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTTcPr;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTTrPr;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTVerticalJc;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.STShd;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.STTblWidth;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.STVerticalJc;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -24,6 +48,13 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class LoginController {
+
+	private GeneracionHL7Service hl7Service;
+
+	@Autowired
+	public void setCService(GeneracionHL7Service hl7Service) {
+		this.hl7Service = hl7Service;
+	}
 
 	@Autowired
 	ConfiguracionRepository configuracionRepository;
@@ -97,6 +128,22 @@ public class LoginController {
 		return "generarInforme";
 	}
 
+	@RequestMapping("/mensajeOMLWizard")
+	public String mensajeOMLWizard(@RequestParam(value = "name", required = false, defaultValue = "World") String name,
+			Model model) {
+		model.addAttribute("mensajeHL7", new MensajeHL7());
+		return "mensajeOMLWizard";
+	}
+
+	@RequestMapping(value = "/ResultadoMensajesHL7", method = RequestMethod.POST)
+	public String ResultadoMensajesHL7(Model model, MensajeHL7 mensaje) {
+		// mensaje.setMensaje(hl7Service.convertirMensajeOML(mensaje.getSistemaOrigen(),
+		// mensaje.getSistemaDestino(), mensaje.getMensaje()));
+		mensaje.setMensaje(hl7Service.convertirMensajeOML(mensaje));
+		model.addAttribute("mensaje", mensaje);
+		return "ResultadoMensajesHL7";
+	}
+
 	@PostMapping("/confirmacionInforme") // //new annotation since 4.3
 	public String confirmacionInforme(@RequestParam("file") MultipartFile file, Model model,
 			RedirectAttributes redirectAttributes, HttpServletRequest request,
@@ -114,11 +161,172 @@ public class LoginController {
 			String nombreFichero = file.getOriginalFilename();
 			mensajeConfirmacion.setMensaje(nombreFichero);
 
+			// Blank Document
+			XWPFDocument document = new XWPFDocument();
+
+			// Write the Document in file system
+			FileOutputStream out = new FileOutputStream(new File("D://TEMP//OUT_Prueba_poi.docx"));
+
+			// Write first Text in the beginning
+			XWPFParagraph para = document.createParagraph();
+			XWPFRun run = para.createRun();
+			run.setText("Hi");
+
+			// create table
+			XWPFTable table = document.createTable();
+
+			table.getCTTbl().addNewTblPr().addNewTblW().setW(BigInteger.valueOf(500));
+
+			// create first row
+			XWPFTableRow tableRowOne = table.getRow(0);
+			tableRowOne.getCell(0).setText("col one, row one");
+			tableRowOne.addNewTableCell().setText("col two, row one");
+			tableRowOne.addNewTableCell().setText("col three, row one");
+
+			// create second row
+			XWPFTableRow tableRowTwo = table.createRow();
+			tableRowTwo.getCell(0).setText("col one, row two");
+			tableRowTwo.getCell(1).setText("col two, row two");
+			tableRowTwo.getCell(2).setText("col three, row two");
+
+			// create third row
+			XWPFTableRow tableRowThree = table.createRow();
+			tableRowThree.getCell(0).setText("col one, row three");
+			tableRowThree.getCell(1).setText("col two, row three");
+			tableRowThree.getCell(2).setText("col three, row three");
+
+			XWPFParagraph para2 = document.createParagraph();
+			XWPFRun run2 = para2.createRun();
+			run2.setText("Bye");
+
+			// Create a new table with 6 rows and 3 columns
+			int nRows = 6;
+			int nCols = 3;
+
+			XWPFTable tableConFormato = document.createTable(nRows, nCols);
+
+			// Set the table style. If the style is not defined, the table style
+			// will become "Normal".
+			CTTblPr tblPr = tableConFormato.getCTTbl().getTblPr();
+			CTString styleStr = tblPr.addNewTblStyle();
+			styleStr.setVal("StyledTable");
+
+			// Get a list of the rows in the table
+			List<XWPFTableRow> rows = tableConFormato.getRows();
+			int rowCt = 0;
+			int colCt = 0;
+			for (XWPFTableRow row : rows) {
+				// get table row properties (trPr)
+				CTTrPr trPr = row.getCtRow().addNewTrPr();
+				// set row height; units = twentieth of a point, 360 = 0.25"
+				CTHeight ht = trPr.addNewTrHeight();
+				ht.setVal(BigInteger.valueOf(360));
+
+				// get the cells in this row
+				List<XWPFTableCell> cells = row.getTableCells();
+				// add content to each cell
+				for (XWPFTableCell cell : cells) {
+					// get a table cell properties element (tcPr)
+					CTTcPr tcpr = cell.getCTTc().addNewTcPr();
+					// set vertical alignment to "center"
+					CTVerticalJc va = tcpr.addNewVAlign();
+					va.setVal(STVerticalJc.CENTER);
+
+					CTTblWidth tblWidth = table.getRow(0).getCell(0).getCTTc().addNewTcPr().addNewTcW();
+					tblWidth.setW(BigInteger.valueOf(2 * 1440));
+					// STTblWidth.DXA is used to specify width in twentieths of
+					// a point.
+					tblWidth.setType(STTblWidth.DXA);
+
+					// create cell color element
+					CTShd ctshd = tcpr.addNewShd();
+					ctshd.setColor("auto");
+					ctshd.setVal(STShd.CLEAR);
+					if (rowCt == 0) {
+						// header row
+						ctshd.setFill("A7BFDE");
+					} else if (rowCt % 2 == 0) {
+						// even row
+						ctshd.setFill("D3DFEE");
+					} else {
+						// odd row
+						ctshd.setFill("EDF2F8");
+					}
+
+					// get 1st paragraph in cell's paragraph list
+					XWPFParagraph paraConFormato = cell.getParagraphs().get(0);
+					// create a run to contain the content
+					XWPFRun rh = paraConFormato.createRun();
+					// style cell as desired
+					if (colCt == nCols - 1) {
+						// last column is 10pt Courier
+						rh.setFontSize(10);
+						rh.setFontFamily("Courier");
+					}
+					if (rowCt == 0) {
+						// header row
+						rh.setText("header row, col " + colCt);
+						rh.setBold(true);
+						para.setAlignment(ParagraphAlignment.CENTER);
+					} else {
+						// other rows
+						rh.setText("row " + rowCt + ", col " + colCt);
+						para.setAlignment(ParagraphAlignment.LEFT);
+					}
+					colCt++;
+				} // for cell
+				colCt = 0;
+				rowCt++;
+			} // for row
+
+			document.write(out);
+			out.close();
+
+			/*
+			 * WordprocessingMLPackage wordMLPackage =
+			 * WordprocessingMLPackage.createPackage();
+			 * 
+			 * String outputfilepath = "D://TEMP/" + "/OUT_Prueba.docx";
+			 * 
+			 * wordMLPackage.getMainDocumentPart().addParagraphOfText("x");
+			 * wordMLPackage.getMainDocumentPart().addParagraphOfText("x");
+			 * wordMLPackage.getMainDocumentPart().addParagraphOfText(
+			 * nombreFichero);
+			 * 
+			 * // create a table int writableWidthTwips =
+			 * wordMLPackage.getDocumentModel().getSections().get(0).
+			 * getPageDimensions() .getWritableWidthTwips(); Tbl tbl =
+			 * TblFactory.createTable(3, 3, writableWidthTwips / 3);
+			 * tbl.getTblGrid().getGridCol();
+			 * 
+			 * 
+			 * Tr headerRow = (Tr) tbl.getContent().get(0);
+			 * 
+			 * int f = 0; for (Column column : tableDef.getColumns()) { Tc
+			 * column = (Tc) headerRow.getContent().get(f); f++; Text text =
+			 * factory.createText(); text.setValue(column.getName()); R run =
+			 * factory.createR(); run.getContent().add(text);
+			 * column.getContent().add(run); headerRow.getContent().add(column);
+			 * }
+			 * 
+			 * wordMLPackage.getMainDocumentPart().addObject(tbl);
+			 * 
+			 * SaveToZipFile saver = new SaveToZipFile(wordMLPackage);
+			 * saver.save(outputfilepath);
+			 */
+
 		} catch (IOException e) {
 			mensajeConfirmacion.setMensaje(e.getMessage());
 			mensajeConfirmacion.setExcepcion(e);
 			e.printStackTrace();
-		}
+		} /*
+			 * catch (InvalidFormatException e) {
+			 * mensajeConfirmacion.setMensaje(e.getMessage());
+			 * mensajeConfirmacion.setExcepcion(e); e.printStackTrace(); } catch
+			 * (Docx4JException e) {
+			 * mensajeConfirmacion.setMensaje(e.getMessage());
+			 * mensajeConfirmacion.setExcepcion(e); e.printStackTrace(); }
+			 */
 		model.addAttribute("mensaje", mensajeConfirmacion);
 		return "confirmacionInforme";
 	}
